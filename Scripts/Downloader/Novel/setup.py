26be1879300a6652439df937e7bcab2ac23135e9
Brent Jeffson F. Florendo <brentjeffson@gmail.com>
2020-08-21 19:26:07 +0800

    added required files for packaging

--- Scripts/Downloader/Novel/setup.py
+++ Scripts/Downloader/Novel/setup.py
@@ -6,5 +6,18 @@ setup(
     description='Downloads Novels',
     author='Brent Jeffson F. Florendo',
     author_email='brentjeffson@gmail.com',
-    packages=find_packages(),
+    packages=find_packages(
+        exclude=('tests', 'tui.py', '*.inf', 'novels')
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='~=3.7',
+    install_requires=[
+        'aiohttp==3.6.2',
+        'beautifulsoup4==4.9.1',
+        'lxml==4.5.2'
+    ]
 )
